26e4a53f7481194dfc8da068dafa02d211ab007b
jessie <57300755+JessicaMa37@users.noreply.github.com>
2022-04-09 19:19:39 -0500

    refactored and cleaned up code per teammate's PR comments

--- moodle/GradientViewController.swift
+++ moodle/GradientViewController.swift
@@ -20,32 +20,23 @@ class GradientViewController: UIViewController {
             defaults.set(darkMode, forKey: darkModeKey)
         }
         gradientLayer.frame = view.bounds
-        gradientLayer.colors = [UIColor(named: "customGradient1")!.cgColor, UIColor(named: "customGradient2")!.cgColor]
-        gradientLayer.shouldRasterize = true
-        view.layer.insertSublayer(gradientLayer, at: 0)
         gradientLayer.startPoint = CGPoint(x: 0, y: 0) // Top left corner.
         gradientLayer.endPoint = CGPoint(x: 1, y: 1) // Bottom right corner.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         let darkModeStatus = defaults.bool(forKey: "darkMode")
+        gradientLayer.removeFromSuperlayer()
+        
         if darkModeStatus {
-            gradientLayer.removeFromSuperlayer()
             overrideUserInterfaceStyle = .dark
-            // Create a gradient layer.
-            
             gradientLayer.colors = [UIColor(red: 0.271, green: 0.263, blue: 0.576, alpha: 1.0).cgColor, UIColor(red: 0.306, green: 0.075, blue: 0.486, alpha: 1.0).cgColor]
-            gradientLayer.shouldRasterize = true
-            view.layer.insertSublayer(gradientLayer, at: 0)
 
-        } else if !darkModeStatus {
-            gradientLayer.removeFromSuperlayer()
+        } else {
             overrideUserInterfaceStyle = .light
-            // Create a gradient layer.
             gradientLayer.colors = [UIColor(named: "customGradient1")!.cgColor, UIColor(named: "customGradient2")!.cgColor]
-            gradientLayer.shouldRasterize = true
-            view.layer.insertSublayer(gradientLayer, at: 0)
         }
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
     func setDarkMode() {
